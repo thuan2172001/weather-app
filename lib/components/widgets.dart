34c9d23a5efc060d86ff11aab8331f6eeb5d0775
thuan2172001 <thuan2172001@gmail.com>
2022-04-15 17:36:46 +0700

    fix: ui

--- lib/components/widgets.dart
+++ lib/components/widgets.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/constants.dart';
 
-
-
 class Div extends StatelessWidget {
   Div({this.height});
   final double height;
@@ -20,12 +18,10 @@ class Div extends StatelessWidget {
 }
 
 class WeatherCards extends StatelessWidget {
-
-  WeatherCards({@required this.text,this.icon});
+  WeatherCards({@required this.text, this.icon});
 
   final String text;
   final String icon;
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +31,9 @@ class WeatherCards extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('images/$icon.png',
-                scale: 8.0),
+            Image.asset('images/$icon.png', scale: 8.0),
             SizedBox(width: 10.0),
-            Text(text,
-                style:kWeatherBoxesText)
+            Text(text, style: kWeatherBoxesText)
           ],
         ),
       ),
